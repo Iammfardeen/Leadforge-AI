6d745658066635eea917f56c57ab50fb29872217
Fardeen Ahmad <161071684+Iammfardeen@users.noreply.github.com>
2026-06-22 16:32:14 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,14 @@ const nextConfig = {
     ],
   },
   typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
